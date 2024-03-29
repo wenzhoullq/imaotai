b3e1ef500d723face5dd171fae5d19857db46de2
li <549108282@qq.com>
2024-03-29 15:22:25 +0800

    v1.1

--- app/dto/item/item.go
+++ app/dto/item/item.go
@@ -1,15 +1,11 @@
 package item
 
-import "time"
-
 type Item struct {
-	ID         int       `json:"id" gorm:"column:id;not null;primary_key;AUTO_INCREMENT;type:int(11)"`
-	Status     int       `json:"status" gorm:"column:status;type:tinyint(4)"`
-	Deleted    int       `json:"deleted" gorm:"column:deleted;type:tinyint(4)"`
-	CreateTime time.Time `json:"create_time" gorm:"column:create_time;not null;default CURRENT_TIMESTAMP;type:timestamp"`
-	UpdateTime time.Time `json:"update_time" gorm:"column:update_time;not null;default CURRENT_TIMESTAMP;ON UPDATE CURRENT_TIMESTAMP;type:timestamp"`
-	ItemCode   string    `json:"item_code" gorm:"column:item_code;type:varchar(20)"`
-	Title      string    `json:"title" gorm:"column:title;type:varchar(20)"`
+	ID       int    `json:"id" gorm:"id"`
+	Status   int    `json:"status" gorm:"status"`
+	Deleted  int    `json:"deleted" gorm:"deleted"`
+	ItemCode string `json:"item_code" gorm:"item_code"`
+	Title    string `json:"title" gorm:"title"`
 }
 
 func (item *Item) TableName() string {
--- app/dto/record/record.go
+++ app/dto/record/record.go
@@ -1,16 +1,12 @@
 package record
 
-import "time"
-
 type Record struct {
-	ID         int       `json:"id" gorm:"column:id;not null;primary_key;AUTO_INCREMENT;type:int(11)"`
-	Deleted    int       `json:"deleted" gorm:"column:deleted;type:tinyint(4)"`
-	CreateTime time.Time `json:"create_time" gorm:"column:create_time;not null;default CURRENT_TIMESTAMP;type:datetime"`
-	UpdateTime time.Time `json:"update_time" gorm:"column:update_time;not null;default CURRENT_TIMESTAMP;ON UPDATE CURRENT_TIMESTAMP;type:datetime"`
-	UserID     int       `json:"user_id" gorm:"column:user_id;type:int(11)"`
-	UserName   string    `json:"user_name" gorm:"column:user_name;type:varchar(20)"`
-	ItemID     string    `json:"item_id" gorm:"column:item_id;type:varchar(50)"`
-	ItemName   string    `json:"item_name" gorm:"column:item_name;type:varchar(100)"`
+	ID       int    `json:"id" gorm:"id"`
+	Deleted  int    `json:"deleted" gorm:"deleted"`
+	UserID   int    `json:"user_id" gorm:"user_id"`
+	UserName string `json:"user_name" gorm:"user_name"`
+	ItemID   string `json:"item_id" gorm:"item_id"`
+	ItemName string `json:"item_name" gorm:"item_name"`
 }
 
 func (rd *Record) TableName() string {
